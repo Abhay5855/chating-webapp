49cd89ee3b3ef473db6694e4bec441d0f980805f
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-05-08 23:46:39 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import styled from "styled-components";
 function App() {
   return (
     <div className="App">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Facebook_Messenger_logo_2018.svg/1200px-Facebook_Messenger_logo_2018.svg.png"
-        alt=""
-        style={{ width: "100px", textalign: "center" }}
-      />
-
       <Container>
         <Chat />
       </Container>
@@ -20,6 +14,7 @@ function App() {
 const Container = styled.div`
   width: 100%;
   height: 100vh;
+  text-align: center;
 `;
 
 export default App;
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,52 +7,44 @@ import { InputLabel } from "@material-ui/core";
 import { Input } from "@material-ui/core";
 import Messages from "../components/Messages";
 import db from "../Firebase";
-import firebase from 'firebase';
+import firebase from "firebase";
 import FlipMove from "react-flip-move";
+import "./Chat.css";
 
 function Chat() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([
-   
-  ]);
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState("");
 
   useEffect(() => {
     setUsername(prompt("Enter your name"));
   }, []);
 
-
   useEffect(() => {
-
-    
-    db.collection('messages')
-    .orderBy('timestamp','desc')
-    .onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => ({id:doc.id , msg:doc.data()} )))
-    })
-
-  },[])
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, msg: doc.data() }))
+        );
+      });
+  }, []);
 
   const sendMessage = (e) => {
     e.preventDefault();
 
-    db.collection('messages').add({
-
-      msg:input,
-      username:username,
+    db.collection("messages").add({
+      msg: input,
+      username: username,
       timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
 
-    })
-
-
-    
-    
     setInput("");
   };
 
   return (
     <Container>
-      <form>
+      <form className="chat__app">
         <FormControl>
           <InputLabel>Enter a message...</InputLabel>
           <Input value={input} onChange={(e) => setInput(e.target.value)} />
@@ -68,11 +60,11 @@ function Chat() {
         </FormControl>
       </form>
 
-     <FlipMove>
-     {messages.map( ({id,msg}) => {
-        return <Messages key={id}  username={username} msg={msg} />;
-      })}
-     </FlipMove>
+      <FlipMove>
+        {messages.map(({ id, msg }) => {
+          return <Messages key={id} username={username} msg={msg} />;
+        })}
+      </FlipMove>
     </Container>
   );
 }
