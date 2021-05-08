d7b93bbefc51c354012d74a6364fb93eb40b23a7
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-05-08 23:30:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Chat from "./components/Chat";
 import styled from "styled-components";
+
 function App() {
   return (
     <div className="App">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Facebook_Messenger_logo_2018.svg/1200px-Facebook_Messenger_logo_2018.svg.png"
+        alt=""
+        style={{ width: "100px", textalign: "center" }}
+      />
+
       <Container>
         <Chat />
       </Container>
